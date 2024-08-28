30c0355a496de2878a2af7d451a158b0562693f9
jaavispeed <ja.rodriguezc@duocuc.cl>
2024-08-28 16:25:48 -0400

    Plantilla registro y validaciones hechas

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { passwordMatch } from '../../models/passwordMatch';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrls: ['./register-page.component.css']
 })
 export default class RegisterPageComponent {
 
+  registerform: FormGroup;
+
+  constructor() {
+    this.registerform = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmpassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+    }, [passwordMatch('password', 'confirmpassword')]);
+  }
 }
