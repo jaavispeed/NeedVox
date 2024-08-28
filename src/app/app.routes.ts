2cf5a208c1c6dfa031047b9b7f43c23cbe36915a
jaavispeed <ja.rodriguezc@duocuc.cl>
2024-08-28 09:46:11 -0400

    Estilo del login con fuente listo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: '/home',
     pathMatch:'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/pages/landing-page/landing-page.component')
   },
   {
     path: 'login',
