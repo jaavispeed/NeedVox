07f5927da2b8514fa866a98cfcaf8fc707f22131
jaavispeed <ja.rodriguezc@duocuc.cl>
2024-10-06 18:28:24 -0300

    Testing e2e en landing Page

--- Proyecto/cypress/e2e/home.cy.ts
+++ Proyecto/cypress/e2e/home.cy.ts
@@ -6,37 +6,33 @@ describe('Página de inicio', () => {
     cy.contains('Needvox').should('exist');
 
     // Verificar que las secciones de Misión, Visión y Propósito estén presentes
-    cy.contains('Misión').should('exist');
-    cy.contains('Visión').should('exist');
-    cy.contains('Propósito').should('exist');
+    cy.contains('Modernización de Almacenes').should('exist');
+    cy.contains('Gestión simplificada del inventario').should('exist');
+    cy.contains('Reportes de ventas en tiempo real').should('exist');
+    cy.contains('Fácil de usar desde cualquier dispositivo').should('exist');
+    cy.contains('Soporte en todo momento').should('exist');
 
     // Verificar que los botones "Ingresar" y "Registrarme" estén presentes
     cy.contains('Ingresar').should('exist');
     cy.contains('Registrarme').should('exist');
 
-
-    // Boton Ingresar
-
+    // Botón Ingresar
     // Hacer clic en el botón "Ingresar" para abrir el pop-up
     cy.contains('Ingresar').click();
 
     // Esperar y verificar que el pop-up de "Ingresar" esté visible
-    cy.get('#login-popup', { timeout: 10000 }).should('be.visible');
+    cy.get('app-login-page', { timeout: 10000 }).should('be.visible');
 
     // Hacer clic en el ícono "X" del pop-up de "Ingresar"
-    cy.get('#login-popup svg path[fill-rule="evenodd"]').should('exist').click(); // Hacemos clic en el ícono "X"
+    cy.get('app-login-page').find('svg path[fill-rule="evenodd"]').should('exist').click(); // Hacemos clic en el ícono "X"
 
-
-    // Boton Register
-
+    // Botón Registrarme
     cy.contains('Registrarme').click();
 
-    // Esperar y verificar que el pop-up de "Ingresar" esté visible
-    cy.get('#login-popup', { timeout: 10000 }).should('be.visible');
+    // Esperar y verificar que el pop-up de "Registro" esté visible
+    cy.get('app-register-page', { timeout: 10000 }).should('be.visible');
 
-    // Hacer clic en el ícono "X" del pop-up de "Ingresar"
-    cy.get('#login-popup svg path[fill-rule="evenodd"]').should('exist').click(); // Hacemos clic en el ícono "X"
-
-
+    // Hacer clic en el ícono "X" del pop-up de "Registro"
+    cy.get('app-register-page').find('svg path[fill-rule="evenodd"]').should('exist').click(); // Hacemos clic en el ícono "X"
   });
 });
