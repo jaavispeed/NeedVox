00698d6700af53b43602e69abc401055dc4879db
jaavispeed <ja.rodriguezc@duocuc.cl>
2024-10-10 22:49:22 -0300

    test del login y registro funcionando

--- Proyecto/cypress/e2e/loginregister.cy.ts
+++ Proyecto/cypress/e2e/loginregister.cy.ts
@@ -5,7 +5,7 @@ describe('Página de inicio', () => {
   });
 
   it('debería mostrar el título, las secciones y los botones correctos', () => {
-    // Pruebas de contenido de la página principal
+    // Verificar el contenido de la página principal
     cy.contains('Needvox').should('exist');
     cy.contains('Modernización de Almacenes').should('exist');
     cy.contains('Gestión simplificada del inventario').should('exist');
@@ -18,25 +18,27 @@ describe('Página de inicio', () => {
 
   describe('Pruebas de registro y login', () => {
     const randomEmail = `usuario_${Math.random().toString(36).substring(2, 7)}@gmail.com`;
-    const username = Math.random().toString(36).substring(2, 7).slice(0, 5); // Generar un nombre de usuario aleatorio de máximo 5 letras
-    const password = '123456'; // Contraseña fija
+    const username = Math.random().toString(36).substring(2, 7).slice(0, 5);
+    const password = '123456';
 
     it('debería registrarse y luego iniciar sesión con las mismas credenciales', () => {
-      // Abrir el pop-up de registro
+      // Abrir el modal de registro
       cy.contains('Registrarme').click();
 
-      // Esperar y verificar que el pop-up de "Registro" esté visible
-      cy.get('#login-popup', { timeout: 10000 }).should('be.visible');
+      // Verificar que el modal de "Registro" esté visible
+      cy.get('app-register-page', { timeout: 10000 }).should('be.visible');
 
       // Completar el formulario de registro
       cy.get('input[name="email"]').type(randomEmail);
       cy.get('input[name="username"]').type(username);
       cy.get('input[name="password"]').type(password);
       cy.get('input[name="confirmpassword"]').type(password);
-      cy.get('button[type="submit"]').click();
+      cy.get('button[type="submit"]').click(); // Hacer clic en el botón de enviar
 
+      // Esperar a que el modal de registro se cierre (si corresponde)
+      cy.get('app-register-page').should('not.exist');
 
-      // Abrir el pop-up de login
+      // Abrir el modal de login
       cy.contains('Ingresar').click();
 
       // Completar el formulario de login con las credenciales usadas para el registro
