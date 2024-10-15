b49cd9bf98e0ae001936a6a1a02adbf301924650
jaavispeed <ja.rodriguezc@duocuc.cl>
2024-10-15 13:57:59 -0300

    Obtener el usuario para el dashboard

--- Proyecto/src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ Proyecto/src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
-export default class DashboardPageComponent {
+export default class DashboardPageComponent implements OnInit {
 
+  userName: string = '';
+
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.obtenerNombreUsuario();
+  }
+
+  obtenerNombreUsuario(): void {
+    this.dashboardService.checkStatus().subscribe(
+      (response) => {
+        this.userName = response.username; // Asumiendo que el backend devuelve el username
+      },
+      (error) => {
+        console.error('Error al verificar el estado del usuario:', error);
+      }
+    );
+  }
 }
---
+++ Proyecto/src/app/dashboard/services/dashboard.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  private apiUrl = 'http://localhost:3000/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  checkStatus(): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    return this.http.get<any>(`${this.apiUrl}/check-status`, { headers });
+  }
+}
