e3e723c9d147dd3df46bfc5405f14ae2dbec95c3
CealStreet <ce.gonzalezv@duocuc.cl>
2024-11-19 21:08:18 -0300

    Modificación Perfil

--- Proyecto/src/app/perfil/perfil.component.ts
+++ Proyecto/src/app/perfil/perfil.component.ts
@@ -4,16 +4,22 @@ import { User } from '../auth/models/user.model';
 import { UsuariosService } from '../admin/pages/totalusers/total-users/usuarios.service'; // Importa el servicio que has creado
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AlertComponent } from '../shared/pages/alert/alert.component';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css'],
   standalone: true,
-  imports: [FormsModule,CommonModule]
+  imports: [FormsModule, CommonModule, AlertComponent],
 })
 export class PerfilComponent implements OnInit {
   user: User | null = null; // Inicia como null
+  alertVisible: boolean = false;
+  alertMessage: string = '';
+  alertType: 'success' | 'error' = 'success';
+  errorMessage: string = '';
+  isEditing: boolean = false; // Nueva variable para el estado de edición
 
   constructor(private authService: AuthService, private usuariosService: UsuariosService) {}
 
@@ -34,15 +40,29 @@ export class PerfilComponent implements OnInit {
         (updatedUser) => {
           // Actualiza los datos de usuario en el componente después de la actualización exitosa
           this.user = updatedUser;
-          alert('Perfil actualizado exitosamente');
+          this.showAlert('Perfil actualizado exitosamente.', 'success');
+          this.isEditing = false; // Cambiar el estado a no editar
         },
         (error) => {
           console.error('Error al actualizar el perfil del usuario:', error);
-          alert('Hubo un error al actualizar el perfil');
+          this.showAlert('Hubo un error al actualizar el perfil.', 'error');
         }
       );
     }
   }
-  
 
+  // Método para activar el modo edición
+  enableEditMode(): void {
+    this.isEditing = true;
+  }
+
+  private showAlert(message: string, type: 'success' | 'error'): void {
+    this.alertMessage = message;
+    this.alertType = type;
+    this.alertVisible = true;
+
+    setTimeout(() => {
+      this.alertVisible = false;
+    }, 3000); // Ocultar el mensaje después de 3 segundos
+  }
 }
--- Proyecto/src/app/perfil/perfil.module.ts
+++ Proyecto/src/app/perfil/perfil.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PerfilComponent } from './perfil.component';
 import { FormsModule } from '@angular/forms';
+import { AlertComponent } from '../shared/pages/alert/alert.component';
 
 @NgModule({
   declarations: [PerfilComponent],
   imports: [
     CommonModule,
     FormsModule, // Asegúrate de importar CommonModule
+    AlertComponent
   ],
   exports: [
     PerfilComponent // Exporta si es necesario
